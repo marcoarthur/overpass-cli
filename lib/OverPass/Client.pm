18868bed10b064460613c177d556a92ecd2822e5
Marco Arthur <arthurpbs@gmail.com>
2021-05-16 10:37:23 -0300

    Client first draft
    First draft can make async/sync requests.

--- lib/OverPass/Client.pm
+++ lib/OverPass/Client.pm
@@ -1,24 +1,67 @@
 package OverPass::Client;
-use 5.008001;
-use strict;
-use warnings;
+use Mojo::URL;
+use Mojo::UserAgent;
+use Carp;
+use Moo;
+use Mojo::Base -async_await, -signatures;
+use namespace::clean;
 
 our $VERSION = "0.01";
 
+has query => (
+    is => 'rw',
+    lazy => 1,
+    default => sub { die "Need a query" },
+);
 
+has base_url => (
+    is => 'ro',
+    default => sub { 
+        Mojo::URL->new('https://lz4.overpass-api.de/api/interpreter') 
+    },
+);
+
+has ua => (
+    is => 'ro',
+    default => sub { Mojo::UserAgent->new },
+);
+
+async sub get_query_p($self, $q = undef) {
+    $self->query( $q ) if $q; # set query if given
+    my $prog = $self->_api_url; # get a clone for base_url
+    $prog->query({ data => $self->query }); # write overpass program
+    return $self->ua->get_p($prog); # execute http get query async
+}
+
+sub get_query( $self, $q = undef ) {
+    my $data;
+    $self->get_query_p($q)->then( sub ($tx) {
+            $data = $tx->res->json;
+        })->catch( sub ($e) {
+            carp "Error $e while retrieving: " . $self->query;
+        })->wait;
+    return $data;
+}
+
+sub _api_url( $self ) {
+    return $self->base_url->clone;
+}
 
 1;
+
 __END__
 
 =encoding utf-8
 
 =head1 NAME
 
-OverPass::Client - It's new $module
+OverPass::Client - Client to Overpass Turbo API (http://overpass-turbo.eu/)
 
 =head1 SYNOPSIS
 
     use OverPass::Client;
+    my $c = OverPass::Client->new;
+    my $data = $c->get_query( "[out:json]; area[name="Caraguatatuba"];node["power"];" );
 
 =head1 DESCRIPTION
 
---
+++ t/01_basic.t
@@ -0,0 +1,39 @@
+use strict;
+use warnings;
+use Test2::V0;
+use OverPass::Client;
+use DDP;
+
+my $c = OverPass::Client->new();
+isa_ok $c, 'OverPass::Client';
+
+# overpass query
+my $q = <<'EOQ';
+[out:json]; //set json as output
+area[name="Ubatuba"]; //search only ubatuba city
+node["power"](area); // node with tag power
+(._;>;);
+EOQ
+
+# query async
+subtest 'query assynchrolously' => sub {
+    my $p = $c->get_query_p( $q );
+    isa_ok $p, 'Mojo::Promise';
+    $p->then( sub { my $tx = shift;
+            ok $tx, 'got a response';
+            isa_ok $tx, 'Mojo::Transaction::HTTP';
+            my $json = $tx->res->json;
+            ok defined $json->{elements}, "has elements in response";
+    })
+    ->catch( sub { fail 'something happended ' . shift} )->wait;
+};
+
+
+# query sync
+subtest 'query osm synchronously' => sub {
+    my $d = $c->get_query($q);
+    ok $d, "got a response";
+    ok $d->{elements}, "contain elements in response";
+};
+
+done_testing;
