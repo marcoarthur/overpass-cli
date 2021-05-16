6f704b1f571a975b14f5cd502d5719bba34893d6
Marco Arthur <arthurpbs@gmail.com>
2021-05-15 23:34:06 -0300

    Initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+OverPass::Client - It's new $module
+
+# SYNOPSIS
+
+    use OverPass::Client;
+
+# DESCRIPTION
+
+OverPass::Client is ...
+
+# LICENSE
+
+Copyright (C) Marco Arthur.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Marco Arthur <arthurpbs@gmail.com>
---
+++ lib/OverPass/Client.pm
@@ -0,0 +1,39 @@
+package OverPass::Client;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+OverPass::Client - It's new $module
+
+=head1 SYNOPSIS
+
+    use OverPass::Client;
+
+=head1 DESCRIPTION
+
+OverPass::Client is ...
+
+=head1 LICENSE
+
+Copyright (C) Marco Arthur.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Marco Arthur E<lt>arthurpbs@gmail.comE<gt>
+
+=cut
+
